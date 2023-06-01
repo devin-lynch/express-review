077d4e37e74cb9e73cce9182d5c2279410831708
devin-lynch <devin.l.lynch@gmail.com>
2023-05-31 19:05:30 -0500

    Test api call

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
 const apiKey = process.env.TCG_API_KEY;
-console.log(apiKey);
+// console.log(apiKey);
 
-app.get('/', (req, res) => {
-    res.send('are we sklookin yet');
+const apiCall = async () => {
+    try {
+        const url = 'https://api.pokemontcg.io/v2/';
+        const response = await fetch(`${url}cards/det1-2`, {
+            headers: {
+                'X-Api-Key': apiKey,
+            },
+        });
+        const data = await response.json();
+        const pokePic = data.data.images.small;
+        console.log(pokePic);
+        return pokePic;
+    } catch (err) {
+        console.warn(err);
+    }
+};
+
+app.get('/', async (req, res) => {
+    const pokePic = await apiCall();
+    res.send(pokePic);
 });
 
 app.get('/one', (req, res) => {
